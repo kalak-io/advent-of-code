f0f3a78a42d5f784dfdf3ee0129f184abae125f4
C Culan <christophe.culan@gmail.com>
2023-01-27 17:14:45 +0100

    proposal using iterators

--- 2022/Day1/part1.py
+++ 2022/Day1/part1.py
@@ -1,18 +1,28 @@
 # -*- coding: utf-8 -*-
+import heapq
+from itertools import groupby
+
 
 INPUT_FILENAME = 'input'
 INPUT_PATH = f'./{INPUT_FILENAME}'
+        
+def elves_iterator(line_iterator):
+    annotated_lines = packet_by_elf_iterator(line_iterator)
+    grouped_by_elf = groupby(annotated_lines, key=lambda item: item[0])
+    return (
+        map(lambda calories_item: calories_item[1], calories_list)
+        for _, calories_list in grouped_by_elf
+    )
 
-if __name__ == '__main__':
-    with open(INPUT_PATH) as f:
-        lines = f.readlines()
-        highTotalCalories = 0
-        currentTotalCalories = 0
-        for line in lines:
-            line = line.replace('\n', '')
-            if not line:
-                highTotalCalories = currentTotalCalories if currentTotalCalories > highTotalCalories else highTotalCalories;
-                currentTotalCalories = 0
-            else:
-                currentTotalCalories = currentTotalCalories + int(line)
-        print(f'The elf with the most calories has {highTotalCalories} calories.')
+def get_calories_per_elf(line_iterator):
+    elves_list = elves_iterator(line_iterator)
+    return (
+        sum(calories_list)
+        for calories_list in elves_list
+    )
+
+
+with open("/home/christophe/Downloads/elves.txt", "r") as f:
+     calories_per_elves = get_calories_per_elf(f)
+     print(sum(heapq.nlargest(3, calories_per_elves)))
+
